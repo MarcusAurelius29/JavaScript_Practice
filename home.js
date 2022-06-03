11671aa54d9528acf2690b9f9fbb2b7b966b7b13
MarcusAurelius29 <59524264+MarcusAurelius29@users.noreply.github.com>
2022-06-04 00:37:29 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,19 @@
+console.log('hello');
+
+//alert('GLORY GLORY MAN UTD');
+
+var b = 'smoothie';
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+//Manipulate DOM with Javascript
+// it is fancy way of saying change HTML with javascript
+
+
+var age = prompt('What is your age?');
+document.getElementById('test').innerHTML = age;
+
+
+//Number in JavaScript
